ad2c953e784f7c4b29910f5a8de709659c6f00b5
mattyturn95 <46969824+mattyturn95@users.noreply.github.com>
2020-07-24 15:29:48 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import Navbar from './components/Navbar/Navbar';
+import './App.scss';
+import Tourlist from './components/TourList';
+
+class App extends Component {
+  render() {
+    return (
+      <main>
+        <Navbar />
+        <Tourlist />
+      </main>
+
+    );
+
+  }
+}
+
+export default App;
+
+
